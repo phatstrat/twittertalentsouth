c2b0ae02f37df3c5e25a2da3b4ae4bd511122084
Jason Byer <jbyer3@gmail.com>
2018-02-08 09:58:32 -0500

    added functionality for hashtags and the ability to follow other users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,15 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+  $('#unfollow_btn').hover(function(){
+    $(this).removeClass('btn-primary');
+    $(this).addClass('btn-danger');
+    $(this).html("Unfollow");
+  }, function(){
+    $(this).html("Following");
+    $(this).removeClass('btn-danger');
+    $(this).addClass('btn-primary');
+  });
+});
---
+++ app/controllers/epicenter_controller.rb
@@ -0,0 +1,71 @@
+class EpicenterController < ApplicationController
+
+  before_action :set_user, only: [:show_user, :following, :followers]
+
+  def all_users
+    @users = User.order(name: :asc)
+  end
+
+  def following
+    empty_users_array
+
+    User.all.each do |user|
+       if @user.following.include?(user.id)
+         @users.push(user)
+       end
+    end
+  end
+
+  def followers
+    empty_users_array
+
+    User.all.each do |user|
+      if user.following.include?(@user.id)
+        @users.push(user)
+      end
+    end
+  end
+
+  def tag_tweets
+    @tag = Tag.find(params[:id])
+  end
+
+  def feed
+    @tweet = Tweet.new
+    @following_tweets = []
+
+    Tweet.all.each do |tweet|
+      if current_user.id == tweet.user_id || current_user.following.include?(tweet.user_id)
+        @following_tweets.push(tweet)
+      end
+    end
+  end
+
+  def show_user
+
+  end
+
+  def now_following
+    current_user.following.push(params[:id].to_i)
+    current_user.save
+
+    redirect_to show_user_path(id: params[:id])
+  end
+
+  def unfollow
+    current_user.following.delete(params[:id].to_i)
+    current_user.save
+
+    redirect_to show_user_path(id: params[:id])
+  end
+
+  private
+
+  def empty_users_array
+    @users = []
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
